44d48dfd1bed3db01f4d8a7d9c93d95a6d4dc4ed
SeyTi01 <jannis.ridder@gmail.com>
2025-07-21 21:58:16 +0200

    [TASK] bump version to 1.8.5

--- src/activityFeedFilter.user.js
+++ src/activityFeedFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Anilist: Activity-Feed Filter
 // @namespace    https://github.com/SeyTi01/
-// @version      1.8.4
+// @version      1.8.5
 // @description  Control the content displayed in your activity feeds
 // @author       SeyTi01
 // @match        https://anilist.co/*
